8a826207ab3afe80db7420d209823f0f48311602
Johnny Gérard <john@jgerard.dev>
2024-11-15 20:36:23 +0100

    docs: add JSDoc to isValidPassword function

--- server/src/validation/password.ts
+++ server/src/validation/password.ts
@@ -1,6 +1,13 @@
 export const PASSWORD_MAX_LENGTH = 100;
 export const PASSWORD_MIN_LENGTH = 8;
 
+/**
+ * Validate a password.
+ *
+ * Note that this validator is trivial and only checks the length of the password.
+ * @param password - The password to validate.
+ * @returns Whether the password is valid.
+ */
 export const isValidPassword = (password: string): boolean =>
   password.length >= PASSWORD_MIN_LENGTH &&
   password.length <= PASSWORD_MAX_LENGTH;
