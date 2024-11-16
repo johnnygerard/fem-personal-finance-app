53c1095d82cd9ca47d169ead6997efd7056e5114
Johnny Gérard <john@jgerard.dev>
2024-11-16 17:33:55 +0100

    feat: add SESSION_COOKIE_NAME to security.ts

--- server/src/constants/security.ts
+++ server/src/constants/security.ts
@@ -2,6 +2,7 @@ import type { CookieOptions } from "express";
 import ms from "ms";
 import { isProduction } from "./env.js";
 
+export const SESSION_COOKIE_NAME = "id";
 export const SESSION_TTL = "2 hours";
 
 export const sessionCookieOptions: CookieOptions = {
