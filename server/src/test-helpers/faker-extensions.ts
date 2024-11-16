d3eb3bb7fd4e2602a6131eb1650f092a8c67cc13
Johnny Gérard <john@jgerard.dev>
2024-11-16 13:44:09 +0100

    test: add getFakeEmail function

--- server/src/test-helpers/faker-extensions.ts
+++ server/src/test-helpers/faker-extensions.ts
@@ -2,6 +2,13 @@ import { faker } from "@faker-js/faker";
 import { PASSWORD_MAX_LENGTH } from "../validation/password.js";
 
 /**
+ * Generate a fake email address with special characters.
+ */
+export const getFakeEmail = (): string => {
+  return faker.internet.email({ allowSpecialCharacters: true });
+};
+
+/**
  * Generate a fake password with random length and characters.
  */
 export const getFakePassword = (): string => {
